ea0339056a3b7e063365a2b19df072bc350d3c6b
Rolando Aburto <rolandoaburto@outlook.com>
2018-02-01 01:26:06 -0300

    Javascript con selectores especiales

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 // Javascript document
 
-function saludo(){
-    alert("Hola usuario");
+function ejecuta(){
+    // for(var i = 0; i<3; i++)
+    // {
+    //     document.getElementsByTagName("figure")[i].onclick=saludo;
+    // }
+    // document.getElementById("figura1").onclick=saludo;
+    // var r = document.getElementsByClassName("figura")[0].onclick=saludo
+
+    // document.querySelector("#division").onclick=saludo;
+    
+    var elementos = document.querySelectorAll(".figura1, span");
+    
+    for (var index = 0; index < elementos.length; index++) {
+        elementos[index].onclick=saludo;    
+    }
+    
 }
+
+function saludo(){
+    alert("Presionaste una imagen");
+}
+
+window.onload=ejecuta;
