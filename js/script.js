505c5238ef4ab62d2af1ec7ed44f6fa8b679ac85
Rolando Aburto <rolandoaburto@outlook.com>
2018-02-02 00:06:27 -0300

    termino de pincelada de Javascript

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // Javascript document
+var imagen = new Array(3);
 
 function ejecuta(){
     // for(var i = 0; i<3; i++)
@@ -10,16 +11,40 @@ function ejecuta(){
 
     // document.querySelector("#division").onclick=saludo;
     
-    var elementos = document.querySelectorAll(".figura1, span");
+    // var elementos = document.querySelectorAll(".figura1, span");
     
-    for (var index = 0; index < elementos.length; index++) {
-        elementos[index].onclick=saludo;    
-    }
+    // for (var index = 0; index < elementos.length; index++) {
+    //     elementos[index].onclick=saludo;    
+    // }
     
 }
 
 function saludo(){
-    alert("Presionaste una imagen");
+    alert("Presionaste una pieza de replica");
 }
 
-window.onload=ejecuta;
+function crecer() {
+    imagen.width=400;
+    imagen.height=400;
+}
+
+function disminuir() {
+    imagen.width=200;
+    imagen.height=200;
+}
+
+function piezasReplica() {
+
+    for(var i = 0; i < 4; i++){
+        imagen[i] = document.getElementsByTagName("img")[i];
+    }
+
+    for(var j=0; j<4; j++){
+        imagen[j].addEventListener("mouseover", crecer, false);
+        imagen[j].addEventListener("mouseout", disminuir, false);
+        imagen[j].addEventListener("click", saludo, false);
+    }
+    
+}
+
+window.addEventListener("load", piezasReplica, false);
